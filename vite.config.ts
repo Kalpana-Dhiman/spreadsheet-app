21ff7b588a1d75d41822f0a000d0de0862dfa078
Kalpana-Dhiman <kalpanadhiman260@gmail.com>
2025-07-04 20:22:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/spreadsheet-app/", // ✅ IMPORTANT
   plugins: [react()],
-  base: "./",
   css: {
     postcss: {
       plugins: [tailwind()],
     },
   },
 });
+
